6721258599d73daf09c37533ee668b12e77a294c
Maria Averina <amaria.webdev@gmail.com>
2022-01-30 00:25:48 +0300

    add Slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
 import './index.css';
 
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slides = document.querySelectorAll('.product');
 
+let index = 0;
+
+const activeSlide = n => {
+  slides.forEach((item) => {
+    item.classList.remove('active');
+  })
+  slides[n].classList.add('active');
+}
+
+const prevSlide = () => {
+  if(index == 0) {
+    index = slides.length - 1;
+    activeSlide(index);
+  } else {
+    index --;
+    activeSlide(index);
+  }
+}
+
+const nextSlide = () => {
+  if(index == slides.length -1) {
+    index = 0;
+    activeSlide(index);
+  } else {
+    index ++;
+    activeSlide(index);
+  }
+}
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
